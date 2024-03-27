724144a474eda3090d463f2808962b889fa113ca
Derek Davis <davismderek@gmail.com>
2024-03-27 12:30:37 -0400

    Debugging

--- fastapi-supabase/db/supabase.py
+++ fastapi-supabase/db/supabase.py
@@ -8,6 +8,8 @@ load_dotenv()
 api_url: str = os.getenv("url")
 key: str = os.getenv("api")
 
+print(api_url)
+
 def create_supabase_client():
     supabase: Client = create_client(api_url, key)
     return supabase
