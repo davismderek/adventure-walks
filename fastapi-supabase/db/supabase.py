26c0c54ea9374175a5b3617ec1b0d7053f0872d8
Derek Davis <davismderek@gmail.com>
2024-03-27 12:49:14 -0400

    Updating keys

--- fastapi-supabase/db/supabase.py
+++ fastapi-supabase/db/supabase.py
@@ -7,7 +7,8 @@ load_dotenv()
 
 # api_url: str = os.getenv("url")
 api_url: str = 'https://ivrkttlvgtjqrxppnrvt.supabase.co'
-key: str = os.getenv("api")
+key: str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml2cmt0dGx2Z3RqcXJ4cHBucnZ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTAxNjUzNzksImV4cCI6MjAyNTc0MTM3OX0.oQEi9eJZVNkdTwl4QvdTxkCg1ij4fwP7dShFntfvTYc"
+
 
 
 def create_supabase_client():
